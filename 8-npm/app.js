02155a2a1ea0343aec8d4a01f47bcfd19e71dbd2
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-10-22 04:16:11 +0700

    menggunakan Package NPM

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,4 @@
-console.log("Hello World");
+const validator = require("validator");
+
+console.log(validator.isEmail("rangga@gmail.com"));
+console.log(validator.isMobilePhone("089526356420", "id-ID"));
