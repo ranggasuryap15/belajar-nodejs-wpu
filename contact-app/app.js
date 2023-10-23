8efc48aa86b6a8e103cbad9654a3dc809a8073ba
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-10-23 09:26:25 +0700

    memasukkan data menggunakan argumen

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,11 +1,37 @@
-const contacts = require("./contact");
+// const contacts = require("./contact");
 
-const main = async () => {
-  const nama = await contacts.pertanyaan("Masukkan nama Anda : ");
-  const phone = await contacts.pertanyaan("Masukkan No HP Anda : ");
-  await contacts.answer();
+// const main = async () => {
+//   const nama = await contacts.pertanyaan("Masukkan nama Anda : ");
+//   const phone = await contacts.pertanyaan("Masukkan No HP Anda : ");
+//   await contacts.answer();
 
-  contacts.saveContact(nama, phone);
-};
+//   contacts.saveContact(nama, phone);
+// };
 
-main();
+// main();
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
+
+yargs(hideBin(process.argv))
+  .command({
+    command: "add",
+    describe: "Menambahkan contact baru",
+    builder: {
+      nama: {
+        describe: "Nama lengkap",
+        demandOption: true,
+        type: "string",
+      },
+      phone: {
+        describe: "Nomor Handphone",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler(argv) {
+      const contacts = require("./contact");
+      contacts.saveContact(argv.nama, argv.phone);
+    },
+  })
+  .parse();
