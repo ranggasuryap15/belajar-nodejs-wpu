2645f5ce908c811aaa8d1d93c4fcdd6aa6a88bed
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-10-23 13:21:25 +0700

    routing dengan express

--- web-server/app.js
+++ web-server/app.js
@@ -1,18 +1,33 @@
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/html" });
 
-  const url = req.url;
-  if (url === "/about") {
-    res.write("<h1>About</h1>");
-  } else if (url === "/contact") {
-    res.write("<h1>Contact</h1>");
-  }
+//   const url = req.url;
+//   if (url === "/about") {
+//     res.write("<h1>About</h1>");
+//   } else if (url === "/contact") {
+//     res.write("<h1>Contact</h1>");
+//   }
 
-  res.end();
+//   res.end();
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server is running...");
+// });
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.get("/about", (req, res) => {
+  res.send("About");
 });
 
-server.listen(3000, () => {
-  console.log("Server is running...");
+app.listen(port, () => {
+  console.log("Example app listening on port 3000!");
 });
